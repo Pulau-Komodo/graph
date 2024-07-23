23275d7ba9eaaf5ee308c6986fbd75a7e3b04c60
Pulau_Komodo <komodo_helico@example.com>
2024-07-23 00:11:40 +0000

    Add public convenience method for making images into png

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,3 +1,5 @@
+use image::{codecs::png::PngEncoder, ColorType, ImageEncoder, RgbImage};
+
 pub mod daily_temp;
 pub mod hourly_composite;
 pub mod hourly_pop;
@@ -6,3 +8,13 @@ pub mod hourly_temp;
 pub mod hourly_uvi;
 pub mod hourly_wind;
 pub mod minutely_precipitation;
+
+pub fn make_png(canvas: RgbImage) -> Vec<u8> {
+	let (width, height) = (canvas.width(), canvas.height());
+	let mut buffer = Vec::new();
+	let encoder = PngEncoder::new(&mut buffer);
+	encoder
+		.write_image(&canvas, width, height, ColorType::Rgb8.into())
+		.unwrap();
+	buffer
+}
