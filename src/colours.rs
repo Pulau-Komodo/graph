f5d387cdf8cfa932f8aded246ac015f580920156
Pulau_Komodo <komodo_helico@example.com>
2024-09-30 19:34:09 +0000

    Make labels white

--- src/colours.rs
+++ src/colours.rs
@@ -3,7 +3,7 @@ pub const BACKGROUND: Rgb<u8> = Rgb([0, 0, 0]);
 pub const MAIN_LINES: Rgb<u8> = Rgb([127, 127, 127]);
 pub const GRID_LINES: Rgb<u8> = Rgb([63, 63, 63]);
 pub const BRIGHTER_GRID_LINES: Rgb<u8> = Rgb([95, 95, 95]);
-pub const TEXT: Rgb<u8> = Rgb([127, 127, 127]);
+pub const TEXT: Rgb<u8> = Rgb([255, 255, 255]);
 
 pub const TEMP_MIN: Rgb<u8> = Rgb([0, 148, 255]);
 pub const TEMP_MAX: Rgb<u8> = Rgb([255, 0, 0]);
