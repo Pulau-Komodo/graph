e05e99882bdfecb6d677af92012f60c655276973
Pulau_Komodo <komodo_helico@example.com>
2024-10-08 20:18:33 +0000

    Attempt to make the text box work a little better
    It still has a problem with not knowing how long the word is going to be if it consists of multiple segments.

--- src/text_box.rs
+++ src/text_box.rs
@@ -20,6 +20,113 @@ impl<'s> TextSegment<'s> {
 	}
 }
 
+struct LineWriter<'s> {
+	lines: Vec<Vec<TextSegment<'s>>>,
+	current_line_width: u32,
+	can_break: bool,
+}
+
+impl<'s> LineWriter<'s> {
+	fn new() -> Self {
+		Self {
+			lines: vec![Vec::new()],
+			current_line_width: 0,
+			can_break: true,
+		}
+	}
+	fn current_line(&self) -> &Vec<TextSegment<'s>> {
+		self.lines.last().unwrap()
+	}
+	fn current_line_mut(&mut self) -> &mut Vec<TextSegment<'s>> {
+		self.lines.last_mut().unwrap()
+	}
+	fn trim_current_line(&mut self) {
+		let trim = |line: &mut [TextSegment]| {
+			let first = &mut line.first_mut().unwrap().text;
+			if let Some(stripped) = first.strip_prefix(' ') {
+				println!("Trimmed start off \"{stripped}\"");
+				*first = stripped;
+			}
+			let last = &mut line.last_mut().unwrap().text;
+			if let Some(stripped) = last.strip_suffix(' ') {
+				println!("Trimmed end off \"{stripped}\"");
+				*last = stripped;
+			}
+		};
+		trim(self.current_line_mut());
+	}
+	fn new_line(&mut self) {
+		self.trim_current_line();
+		self.lines.push(Vec::new());
+		self.current_line_width = 0;
+	}
+	fn add_to_current_line(&mut self, segment: TextSegment<'s>, width: u32) {
+		//println!("Adding \"{}\"", segment.text);
+		self.current_line_mut().push(segment);
+		self.current_line_width += width;
+	}
+	fn add_segment(
+		&mut self,
+		segment: TextSegment<'s>,
+		font: &FontRef<'_>,
+		scale: PxScale,
+		width: u32,
+	) {
+		let mut start_index = 0;
+		let mut prev_break_point = None;
+		for index in segment
+			.text
+			.char_indices()
+			.filter_map(|(index, char)| (char == ' ').then_some(index))
+		{
+			self.can_break = true;
+			let segment_width =
+				drawing::text_size(scale, &font, &segment.text[start_index..index]).0;
+			let line_width = self.current_line_width + segment_width;
+			if line_width <= width {
+				prev_break_point = Some(index);
+				continue;
+			}
+
+			let end_index = prev_break_point.filter(|i| *i >= start_index).unwrap_or(index);
+			let new_segment = &segment.text[start_index..end_index];
+			if !new_segment.is_empty() {
+				self.add_to_current_line(
+					TextSegment {
+						text: new_segment,
+						color: segment.color,
+					},
+					0,
+				);
+			}
+			self.new_line();
+			prev_break_point = Some(index);
+			start_index = end_index + 1;
+			if start_index >= segment.text.len() {
+				return;
+			}
+		}
+		let remainder = &segment.text[start_index..];
+		if !remainder.is_empty() {
+			let segment_width = drawing::text_size(scale, &font, remainder).0;
+			let line_width = self.current_line_width + segment_width;
+			if line_width > width && self.can_break && !self.current_line().is_empty() {
+				self.new_line();
+			}
+			self.add_to_current_line(
+				TextSegment {
+					text: remainder,
+					color: segment.color,
+				},
+				segment_width,
+			);
+			if !remainder.ends_with(' ') {
+				self.can_break = false;
+			}
+		}
+	}
+}
+
 #[derive(Debug, Clone)]
 pub struct TextBox<'f, 's> {
 	lines: Vec<Vec<TextSegment<'s>>>,
@@ -37,60 +144,19 @@ impl<'f, 's> TextBox<'f, 's> {
 		width: u32,
 		line_distance: u32,
 	) -> Self {
-		let mut lines = Vec::new();
-		let mut current_line = Vec::new();
-		let mut current_line_width = 0;
+		let mut lines = LineWriter::new();
 		for segment in text {
-			let mut start_index = 0;
-			let mut prev_space_index = 0;
-			for index in segment
-				.text
-				.char_indices()
-				.filter_map(|(index, char)| (char == ' ').then_some(index))
-				.chain([segment.text.len()])
-			{
-				let segment_width =
-					drawing::text_size(font_scale, &font, &segment.text[start_index..index]).0;
-				let line_width = current_line_width + segment_width;
-				if line_width > width {
-					let new_segment = &segment.text[start_index..prev_space_index];
-					if new_segment.is_empty() {
-						if !current_line.is_empty() {
-							lines.push(current_line);
-							current_line = Vec::new();
-							current_line_width = 0;
-						}
-						continue;
-					}
-					current_line.push(TextSegment {
-						text: new_segment,
-						color: segment.color,
-					});
-					lines.push(current_line);
-					current_line = Vec::new();
-					current_line_width = 0;
-					start_index = prev_space_index + 1;
-				} else {
-					prev_space_index = index;
-				}
-			}
-			let remainder = &segment.text[start_index..];
-			if !remainder.is_empty() {
-				let segment_width = drawing::text_size(font_scale, &font, remainder).0;
-				current_line_width += segment_width;
-				current_line.push(TextSegment {
-					text: remainder,
-					color: segment.color,
-				});
-			}
+			lines.add_segment(*segment, &font, font_scale, width);
 		}
 
-		if !current_line.is_empty() {
-			lines.push(current_line);
+		if lines.current_line().is_empty() {
+			lines.lines.pop();
+		} else {
+			lines.trim_current_line();
 		}
 
 		Self {
-			lines,
+			lines: lines.lines,
 			font,
 			font_scale,
 			line_distance,
@@ -140,31 +206,13 @@ mod tests {
 		let font_data: &[u8] = include_bytes!("../RobotoCondensed-Regular.ttf");
 		let font = ab_glyph::FontRef::try_from_slice(font_data).expect("Failed to read font");
 
-		let segments = vec![
-			TextSegment {
-				text: "Minimum",
-				color: Rgb([0, 148, 255]),
-			},
-			TextSegment {
-				text: ", ",
-				color: Rgb([255, 255, 255]),
-			},
-			TextSegment {
-				text: "maximum",
-				color: Rgb([255, 0, 0]),
-			},
-			TextSegment {
-				text: " and ",
-				color: Rgb([255, 255, 255]),
-			},
-			TextSegment {
-				text: "apparent minimum and maximum",
-				color: Rgb([0, 170, 33]),
-			},
-			TextSegment {
-				text: " temperatures (°C)",
-				color: Rgb([255, 255, 255]),
-			},
+		let segments = [
+			TextSegment::new("Minimum", Rgb([0, 148, 255])),
+			TextSegment::white(", "),
+			TextSegment::new("maximum", Rgb([255, 0, 0])),
+			TextSegment::white(" and "),
+			TextSegment::new("apparent minimum and maximum", Rgb([0, 170, 33])),
+			TextSegment::white(" temperatures (°C)"),
 		];
 		let text_box = TextBox::new(&segments, font, PxScale { x: 15.0, y: 15.0 }, 151, 5);
 		for line in text_box.lines {
@@ -176,31 +224,13 @@ mod tests {
 		let font_data: &[u8] = include_bytes!("../RobotoCondensed-Regular.ttf");
 		let font = ab_glyph::FontRef::try_from_slice(font_data).expect("Failed to read font");
 
-		let segments = vec![
-			TextSegment {
-				text: "Minimum",
-				color: Rgb([0, 148, 255]),
-			},
-			TextSegment {
-				text: ", ",
-				color: Rgb([255, 255, 255]),
-			},
-			TextSegment {
-				text: "maximum",
-				color: Rgb([255, 0, 0]),
-			},
-			TextSegment {
-				text: " and ",
-				color: Rgb([255, 255, 255]),
-			},
-			TextSegment {
-				text: "apparent minimum and maximum",
-				color: Rgb([0, 170, 33]),
-			},
-			TextSegment {
-				text: " temperatures (°C)",
-				color: Rgb([255, 255, 255]),
-			},
+		let segments = [
+			TextSegment::new("Minimum", Rgb([0, 148, 255])),
+			TextSegment::white(", "),
+			TextSegment::new("maximum", Rgb([255, 0, 0])),
+			TextSegment::white(" and "),
+			TextSegment::new("apparent minimum and maximum", Rgb([0, 170, 33])),
+			TextSegment::white(" temperatures (°C)"),
 		];
 		let text_box = TextBox::new(&segments, font, PxScale { x: 15.0, y: 15.0 }, 151, 5);
 		let mut chart = Chart::new(
@@ -220,5 +250,53 @@ mod tests {
 		chart.draw(text_box);
 		let canvas = chart.into_canvas();
 		let _ = canvas.save("./test.png");
+	}
+	#[test]
+	fn big_test() {
+		let font_data: &[u8] = include_bytes!("../RobotoMono-Regular.ttf");
+		let font = ab_glyph::FontRef::try_from_slice(font_data).expect("Failed to read font");
+
+		let scale = PxScale { x: 15.0, y: 15.0 };
+		let test_text = "The quick brown fox jumped over the lazy dog.";
+		let char_width =
+			drawing::text_size(scale, &font, test_text).0 as f32 / test_text.len() as f32;
+		println!("char_width: {char_width}");
+
+		let segments = [
+			TextSegment::white("The quick "),
+			TextSegment::new("brown", Rgb([150, 75, 0])),
+			TextSegment::white(" fox jumped over the "),
+			TextSegment::new("l", Rgb([200, 200, 255])),
+			TextSegment::new("a", Rgb([100, 100, 255])),
+			TextSegment::new("z", Rgb([200, 200, 255])),
+			TextSegment::new("y", Rgb([100, 100, 255])),
+			TextSegment::white(" dog."),
+		];
+
+		for i in 1..=45 {
+			// if i != 3 {
+			// 	continue;
+			// }
+			println!("{i}");
+			let width = (i as f32 * char_width as f32) as u32;
+			let text_box = TextBox::new(&segments, font.clone(), scale, width, 0);
+			let mut chart = Chart::new(
+				2,
+				0,
+				Spacing {
+					horizontal: width,
+					vertical: 0,
+				},
+				Padding {
+					above: text_box.height(),
+					below: 0,
+					left: 0,
+					right: 0,
+				},
+			);
+			chart.draw(text_box);
+			let canvas = chart.into_canvas();
+			let _ = canvas.save(format!("./test-{i}.png"));
+		}
 	}
 }
