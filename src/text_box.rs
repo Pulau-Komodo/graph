0bea23a3b96b01795fbcdd819d47c6ba5f73cfcd
Pulau_Komodo <komodo_helico@example.com>
2025-05-26 21:27:32 +0000

    Less wrong

--- src/text_box.rs
+++ src/text_box.rs
@@ -73,11 +73,12 @@ impl<'s> LineWriter<'s> {
 		self.current_word.push(segment);
 		self.current_word_width += width;
 	}
+	/// Write current word to the current line. Harmless if current word is empty.
 	fn add_current_word(&mut self) {
-		for segment in std::mem::take(&mut self.current_word).drain(..) {
+		for segment in std::mem::take(&mut self.current_word) {
 			self.current_line_mut().push(segment);
 		}
-		self.current_line_width += std::mem::take(&mut self.current_line_width);
+		self.current_line_width += std::mem::take(&mut self.current_word_width);
 	}
 	fn add_segment(
 		&mut self,
@@ -94,16 +95,27 @@ impl<'s> LineWriter<'s> {
 			.filter_map(|(index, char)| (char == ' ').then_some(index))
 		{
 			self.can_break = true;
+			if self.current_word_width + self.current_line_width > width
+				&& !self.current_line().is_empty()
+				&& !self.current_word.is_empty()
+			{
+				self.new_line();
+			}
 			self.add_current_word();
 			let segment_width =
 				drawing::text_size(scale, &font, &segment.text[start_index..index]).0;
 			let line_width = self.current_line_width + segment_width;
 			if line_width <= width {
+				// Short enough; keep going.
 				prev_break_point = Some(index);
 				continue;
 			}
+			// Too long; finish line and start a new one.
 
-			let end_index = prev_break_point.filter(|i| *i >= start_index).unwrap_or(index);
+			// Go to a break point previously found if it's actually before the current point, otherwise just go to current index.
+			let end_index = prev_break_point
+				.filter(|i| *i >= start_index)
+				.unwrap_or(index);
 			let new_segment = &segment.text[start_index..end_index];
 			if !new_segment.is_empty() {
 				self.add_to_current_line(
@@ -121,27 +133,42 @@ impl<'s> LineWriter<'s> {
 				return;
 			}
 		}
+
 		let remainder = &segment.text[start_index..];
-		if !remainder.is_empty() {
-			let segment_width = drawing::text_size(scale, &font, remainder).0;
-			if self.can_break {
-				self.add_current_word();
-			}
-			let line_width = self.current_line_width + segment_width;
-			if line_width > width && self.can_break && !self.current_line().is_empty() {
-				self.new_line();
-			}
-			self.add_to_current_word(
-				TextSegment {
-					text: remainder,
-					color: segment.color,
-				},
-				segment_width,
-			);
-			if !remainder.ends_with(' ') {
-				self.can_break = false;
-			}
+		if remainder.is_empty() {
+			return;
 		}
+
+		let segment_width = drawing::text_size(scale, &font, remainder).0;
+		if self.can_break {
+			self.add_current_word();
+		}
+		let line_width = self.current_line_width + segment_width;
+		if line_width > width && self.can_break && !self.current_line().is_empty() {
+			self.new_line();
+			self.can_break = false;
+		}
+		self.add_to_current_word(
+			TextSegment {
+				text: remainder,
+				color: segment.color,
+			},
+			segment_width,
+		);
+		if !remainder.ends_with(' ') {
+			self.can_break = false;
+		} else {
+			//	self.can_break = true;
+		}
+	}
+	fn finish(mut self) -> Vec<Vec<TextSegment<'s>>> {
+		self.add_current_word();
+		if self.current_line().is_empty() {
+			self.lines.pop();
+		} else {
+			self.trim_current_line();
+		}
+		self.lines
 	}
 }
 
@@ -162,20 +189,14 @@ impl<'f, 's> TextBox<'f, 's> {
 		width: u32,
 		line_distance: u32,
 	) -> Self {
-		let mut lines = LineWriter::new();
+		let mut line_writer = LineWriter::new();
 		for segment in text {
-			lines.add_segment(*segment, &font, font_scale, width);
+			line_writer.add_segment(*segment, &font, font_scale, width);
 		}
-
-		lines.add_current_word();
-		if lines.current_line().is_empty() {
-			lines.lines.pop();
-		} else {
-			lines.trim_current_line();
-		}
+		let lines = line_writer.finish();
 
 		Self {
-			lines: lines.lines,
+			lines,
 			font,
 			font_scale,
 			line_distance,
@@ -315,7 +336,58 @@ mod tests {
 			);
 			chart.draw(text_box);
 			let canvas = chart.into_canvas();
-			let _ = canvas.save(format!("./test-{i}.png"));
+			if image::open(format!("./test-{i}.png"))
+				.unwrap()
+				.as_rgb8()
+				.unwrap() != &canvas
+			{
+				println!("{i} changed.");
+				let _ = canvas.save(format!("./test-{i}-new.png"));
+			}
+		}
+	}
+	#[test]
+	fn test_attached_colours() {
+		let font_data: &[u8] = include_bytes!("../RobotoMono-Regular.ttf");
+		let font = ab_glyph::FontRef::try_from_slice(font_data).expect("Failed to read font");
+
+		let scale = PxScale { x: 15.0, y: 15.0 };
+		let test_text = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let char_width =
+			drawing::text_size(scale, &font, test_text).0 as f32 / test_text.len() as f32;
+		println!("char_width: {char_width}");
+
+		let mut color = Rgb([255, 0, 0]);
+		let segments: Vec<_> = test_text
+			.char_indices()
+			.skip(1)
+			.map(|(index, _)| {
+				color.0[0] -= 4;
+				color.0[2] += 4;
+				TextSegment::new(&test_text[index - 1..index], color)
+			})
+			.collect();
+
+		for i in (1..=10).chain([test_text.len()]) {
+			let width = (i as f32 * char_width as f32) as u32;
+			let text_box = TextBox::new(&segments, font.clone(), scale, width, 0);
+			let mut chart = Chart::new(
+				2,
+				0,
+				Spacing {
+					horizontal: width,
+					vertical: 0,
+				},
+				Padding {
+					above: text_box.height(),
+					below: 0,
+					left: 0,
+					right: 0,
+				},
+			);
+			chart.draw(text_box);
+			let canvas = chart.into_canvas();
+			let _ = canvas.save(format!("./test2-{i}.png"));
 		}
 	}
 }
